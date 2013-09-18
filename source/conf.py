fb5aec3b529ac1ee7e8849be72b50e8195137a83
Gilles FELIX <felix@novius.com>
2013-09-18 09:30:31 +0200

    Add noviusos theme submodule

--- source/conf.py
+++ source/conf.py
@@ -98,23 +98,21 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'noviusos'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-	"sidebarwidth": 300,
-	"stickysidebar": "true",
-	"externalrefs": "true"
+	"sidebarwidth": 300
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['_theme']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'API documentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
